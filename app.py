5e78a0c39c5663d85b559f0bbf8a9e4779d673ac
snay <feligges@gmx.de>
2020-09-29 11:41:29 +0200

    created basic dash plotly setup, imported both df

---
+++ app.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+# Run this app with `python app.py` and
+# visit http://127.0.0.1:8050/ in your web browser.
+
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import plotly.express as px
+import pandas as pd
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+df_episodes = pd.read_csv('_RES/data/Dolores_All_Episodes.csv', index_col=0)
+df_shows = pd.read_csv('_RES/data/Dolores_TV_Shows.csv', index_col=0)
+
+fig = px.bar(df_shows, x="title", y="imdb_rating", color="genre")
+
+app.layout = html.Div(children=[
+    html.H1(children='Hello Dash'),
+
+    html.Div(children='''
+        Dash: A web application framework for Python.
+    '''),
+
+    dcc.Graph(
+        id='example-graph',
+        figure=fig
+    )
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
